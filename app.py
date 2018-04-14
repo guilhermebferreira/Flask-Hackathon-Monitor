fc44bb52758226f473e731f919671b18d90b0e6a
Guilherme Ferreira <360guilherme@gmail.com>
2018-04-14 13:55:54 -0300

    OAth github authentincation

--- app.py
+++ app.py
@@ -1,20 +1,52 @@
-# -*- coding: utf-8 -*-
-
-import os
-import random
-import sys
-import json
-
-import requests
-from flask import Flask, request
-
-
-reload(sys)
-sys.setdefaultencoding('utf8')
+from requests_oauthlib import OAuth2Session
+from flask import Flask, request, redirect, session, url_for, json
+from flask.json import jsonify
+import os, sys, requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+app.secret_key = "super secret key"
+
+# This information is obtained upon registration of a new GitHub
+
+client_id = os.environ['client_id']
+client_secret = os.environ['client_secret']
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+
+
+@app.route("/")
+def demo():
+    github = OAuth2Session(client_id)
+    authorization_url, state = github.authorization_url(authorization_base_url)
+
+    session['oauth_state'] = state
+    return redirect(authorization_url)
+
+
+# Step 2: User authorization, this happens on the provider.
+
+@app.route("/callback", methods=["GET"])
+def callback():
+
+    github = OAuth2Session(client_id, state=session['oauth_state'])
+    token = github.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.url)
+
+    session['oauth_token'] = token
+
+    return redirect(url_for('.profile'))
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Fetching a protected resource using an OAuth 2 token.
+    """
+    github = OAuth2Session(client_id, token=session['oauth_token'])
+    return jsonify(github.get('https://api.github.com/user').json())
+
+
+@app.route('/repo')
 def repo():
     r = requests.get('https://api.github.com/repos/django/django')
     if (r.ok):
@@ -23,9 +55,13 @@ def repo():
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
+    print(str(message))
     sys.stdout.flush()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # This allows us to use a plain HTTP callback
+
+    os.environ['DEBUG'] = "1"
+
     app.run(debug=True)
